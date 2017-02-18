05749415f80b54a3fac319c9620f53854269afa9
rvinitraumass <rvinitra@cs.umass.edu>
2017-02-17 22:48:03 -0500

    Final changes to strings.py with all test cases passing

--- 04 - Strings/strings.py
+++ 04 - Strings/strings.py
@@ -11,7 +11,7 @@ def print_strings(file_obj, encoding, min_len, print_all):
             if len(byte) == 2 and (0x20 <= byte[0] <= 0x7e) and byte[1] == 0x00:
                 count += 1
                 printable.append(byte.decode(encoding='utf-16-le'))
-            elif len(byte) == 2 and print_all and (0xa1 <= byte[0] <= 0xff) and (0x00 <= byte[1] <= 0xd7):
+            elif len(byte) == 2 and print_all and ((0xa1 <= byte[0] <= 0xff and 0x00 == byte[1]) or (0x00 <= byte[0] <= 0xff and 0x00 < byte[1] <= 0xd7)):
                 count += 1
                 printable.append(byte.decode(encoding='utf-16-le'))
             else:
@@ -28,7 +28,7 @@ def print_strings(file_obj, encoding, min_len, print_all):
             if len(byte) == 2 and (0x20 <= byte[1] <= 0x7e) and byte[0] == 0x00:
                 count += 1
                 printable.append(byte.decode(encoding='utf-16-be'))
-            elif len(byte) == 2 and print_all and (0xa1 <= byte[1] <= 0xff) and (0x00 <= byte[0] <= 0xd7):
+            elif len(byte) == 2 and print_all and ((0xa1 <= byte[1] <= 0xff and 0x00 == byte[0]) or (0x00 <= byte[1] <= 0xff and 0x00 < byte[0] <= 0xd7)):
                 count += 1
                 printable.append(byte.decode(encoding='utf-16-be'))
             else:
@@ -45,28 +45,114 @@ def print_strings(file_obj, encoding, min_len, print_all):
             flag = False
             if (0x20 <= byte[0] <= 0x7e):
                 count += 1
-                printable.append(byte.decode(encoding='utf-8'))
+                printable.append(byte[0:1].decode(encoding='utf-8'))
                 flag = True
-            if print_all and not flag and (0xc2 <= byte[0] <= 0xdf):
+                if len(byte) > 1:
+                    byte = byte[1:]
+                    continue
+            if print_all and not flag and byte[0] == 0xc2:
                 byte+=file_obj.read(1)
-                if len(byte) == 2 and (byte[1] & 0xc0 == 0x80):
+                if len(byte) == 2 and 0xa1 <= byte[1] <= 0xbf:
                     count += 1
                     printable.append(byte.decode(encoding='utf-8'))
                     flag = True
-            if print_all and not flag and (0xe0 <= byte[0] <= 0xed):
+                elif len(byte) == 2:
+                    if count >= min_len:
+                        print(''.join(printable))
+                    count = 0
+                    printable = []
+                    byte = byte[1:]
+                    continue
+            if print_all and not flag and (0xc2 < byte[0] <= 0xdf):
+                byte+=file_obj.read(1)
+                if len(byte) == 2 and (0x80 <= byte[1] <= 0xbf):
+                    count += 1
+                    printable.append(byte.decode(encoding='utf-8'))
+                    flag = True
+                elif len(byte) == 2:
+                    if count >= min_len:
+                        print(''.join(printable))
+                    count = 0
+                    printable = []
+                    byte = byte[1:]
+                    continue
+            if print_all and not flag and byte[0] == 0xe0:
                 if len(byte) != 2:
                     byte += file_obj.read(1)
-                if len(byte) == 2 and (byte[1] & 0xc0 == 0x80):
+                if len(byte) == 2 and 0xa0 <= byte[1] <= 0xbf:
                     byte += file_obj.read(1)
-                    if len(byte) == 3 and (byte[2] & 0xc0 == 0x80):
+                    if len(byte) == 3 and 0x80 <= byte[2] <= 0xbf:
                         count += 1
                         printable.append(byte.decode(encoding='utf-8'))
                         flag = True
+                    elif len(byte) == 3:
+                        if count >= min_len:
+                            print(''.join(printable))
+                        count = 0
+                        printable = []
+                        byte = byte[1:]
+                        continue
+                elif len(byte) == 2:
+                    if count >= min_len:
+                        print(''.join(printable))
+                    count = 0
+                    printable = []
+                    byte = byte[1:]
+                    continue
+            if print_all and not flag and (0xe0 < byte[0] < 0xed):
+                if len(byte) != 2:
+                    byte += file_obj.read(1)
+                if len(byte) == 2 and (0x80 <= byte[1] <= 0xbf):
+                    byte += file_obj.read(1)
+                    if len(byte) == 3 and (0x80 <= byte[2] <= 0xbf):
+                        count += 1
+                        printable.append(byte.decode(encoding='utf-8'))
+                        flag = True
+                    elif len(byte) == 3:
+                        if count >= min_len:
+                            print(''.join(printable))
+                        count = 0
+                        printable = []
+                        byte = byte[1:]
+                        continue
+                elif len(byte) == 2:
+                    if count >= min_len:
+                        print(''.join(printable))
+                    count = 0
+                    printable = []
+                    byte = byte[1:]
+                    continue
+            if print_all and not flag and byte[0] == 0xed:
+                if len(byte) != 2:
+                    byte += file_obj.read(1)
+                if len(byte) == 2 and 0x80 <= byte[1] <= 0x9f:
+                    byte += file_obj.read(1)
+                    if len(byte) == 3 and 0x80 <= byte[2] <= 0xbf:
+                        count += 1
+                        printable.append(byte.decode(encoding='utf-8'))
+                        flag = True
+                    elif len(byte) == 3:
+                        if count >= min_len:
+                            print(''.join(printable))
+                        count = 0
+                        printable = []
+                        byte = byte[1:]
+                        continue
+                elif len(byte) == 2:
+                    if count >= min_len:
+                        print(''.join(printable))
+                    count = 0
+                    printable = []
+                    byte = byte[1:]
+                    continue
             if not flag:
                 if count >= min_len:
                     print(''.join(printable))
                 count = 0
                 printable = []
+                if len(byte) > 1:
+                    byte = byte[1:]
+                    continue
             byte = file_obj.read(1)
         if count >= min_len:
             print(''.join(printable))
